a151a6dbc49ff13df5b09754718de395f7f0bf7b
Umut Berhan <umutberhan94@gmail.com>
2022-04-29 13:33:52 +0300

    some changes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,20 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
+import TypeAnimation from 'react-type-animation';
+import { useTheme } from 'next-themes'
+
+const ThemeChanger = () => {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <div>
+      <button onClick={() => setTheme('light')}>Light Mode</button> {' '}
+      <button onClick={() => setTheme('dark')}>Dark Mode</button>
+    </div>
+  )
+}
+
 
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
@@ -31,7 +45,28 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Umut. I'm a front-end developer. I'm learning Next.js to build modern web applications. I hope that it's going to be a fun learning journey! 😊</p>
+        <ThemeChanger />
+        {/* <TypeAnimation
+          cursor={false}
+          sequence={["Hello, I'm Umut. I'm a front-end developer. I'm learning Next.js to build modern web applications. I hope that it's going to be a fun learning journey!", 1000, '']}
+          wrapper="h2"
+        /> */}
+
+        <TypeAnimation
+         cursor={true}
+         sequence={[
+           "Hello, I'm Umut.",
+           1500,
+           "I'm a front-end developer.",
+           1500,
+           "I'm learning Next.js to build modern web applications.",
+           1500,
+           "I hope that it's going to be a fun learning journey!",
+           5000
+         ]}
+         wrapper="h2"
+         repeat={Infinity}
+        />
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
